0677f2eea20f2775d32ab316abc1b9edfe80b9a6
Nawodya Ishan <nawodyain@gmail.com>
2023-11-02 22:13:40 +0530

    feat: add docker config files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({message: 'Yeah! Docker baby !'});
+    res.json({message: 'Yeah! Docker baby 🐳'});
 });
 
 app.listen(PORT, () => {
